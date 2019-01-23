ca5115b2518935348928a4ad8911322ad8e7ae52
Macolbar <elgithubdemacolbar@gmail.com>
2019-01-23 21:35:40 +0100

    segundo commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+export const appconfig = {
+    firebase: {
+    apiKey: "AIzaSyBCWROaqlzbsOx06yMxkqe_CAqIoOOeo34",
+    authDomain: "chatfire-base.firebaseapp.com",
+    databaseURL: "https://chatfire-base.firebaseio.com",
+    projectId: "chatfire-base",
+    storageBucket: "",
+    messagingSenderId: "266545689916"
+    },
+    users_endpoint: "users",
+    chats_endpoint: "chats"
+  };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,43 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule, AngularFirestore, AngularFirestoreCollection } from "angularfire2/firestore";
+
+import { appconfig } from "./app.config";
+import { ChatService } from './app.service';
+import { ChatsPage } from '../pages/chats/chats';
+import { ChatroomPage } from '../pages/chatroom/chatroom';
+
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ChatsPage,
+    ChatroomPage
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(appconfig.firebase),
+    AngularFirestoreModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ChatsPage,
+    ChatroomPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ChatService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
+
